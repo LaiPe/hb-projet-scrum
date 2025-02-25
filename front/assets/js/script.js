366b86886fec3eed45f42989e09e947f7f8570ea
Vincent-Gillet <vincent.gillet.vg@gmail.com>
2025-02-25 13:47:51 +0100

    modif script

--- front/assets/js/script.js
+++ front/assets/js/script.js
@@ -1,20 +1,45 @@
+let form = document.getElementById('form');
+let pseudo = document.getElementById('pseudo');
+let password = document.getElementById('password');
+
 let url = 'http://localhost:3000/api/v1/users';
 let token = localStorage.getItem('token');
 
-fetch(url, {
-    "GET",
-    headers: {
-        "Authorization": `Bearer ${token}` 
-    }
-}).then(res => res.json().then(console.log)).catch(console.error); 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-if (token !== null) {
     fetch(url, {
-        method,
+        method: 'POST',
+        body: JSON.stringify({
+            pseudo: pseudo.value,
+            password: password.value
+        }),
+    }).then(res => res.json()
+    .then(
+        
+    ))
+    .catch(console.error);     
+});
+
+
+
+fetch(url, {
+    method: 'GET',
+}).then(res => res.json()
+.then(console.log))
+.catch(console.error); 
+
+if (token) {
+    fetch(url, {
+        method: 'POST',
         headers: {
             "Authorization": `Bearer ${token}`
         }
-    }).then(res => res.json().then(console.log)).catch(console.error); 
+    }).then(res => res.json()
+    .then(console.log))
+    .catch(console.error); 
+
+
 } else {
     window.location.href = '../../index.html';
 }
