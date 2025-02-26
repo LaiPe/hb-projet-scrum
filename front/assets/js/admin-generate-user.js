c53432aeb776353e21ea947a9222d95884aec02d
Léo Peyronnet Laptop <peyronnet.leo@gmail.com>
2025-02-26 12:00:31 +0100

    feat: script post form admin & fetch response

--- front/assets/js/admin-generate-user.js
+++ front/assets/js/admin-generate-user.js
@@ -1,21 +1,41 @@
-let form = document.querySelector("#form");
+const form = document.querySelector("#form");
 
-let username = document.querySelector("#username");
-let username_create = document.querySelector("#username_create");
-let password = document.querySelector("#password");
+const username_input = document.querySelector("#username");
 
-let password_value = "Lorem";
-
-console.log(password_value);
+const username_text = document.querySelector("#username_create");
+const password_text = document.querySelector("#password");
+const error_text = document.querySelector("#error");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    username.innerHTML = "";
-    username_create.innerHTML = "";
-    
-    if (form.username.value != "") {
-        username_create.innerHTML = `${ form.username.value }`;
-        password.innerHTML = `${ password_value }`;
-    }
+    username_text.textContent = "";
+    password_text.textContent = "";
+    error_text.textContent = "";
+
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erreur réseau");
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.username && data.password) {
+            username_text.textContent = data.username;
+            password_text.textContent = data.password;
+        } else {
+            error_text.textContent = "Erreur lors de la génération, veuillez consulter le support."
+            console.error("Réponse JSON invalide");
+        }
+    })
+    .catch(error => {
+        error_text.textContent = "Erreur lors de la génération, veuillez consulter le support."
+        console.error("Erreur lors de la requête:", error);
+    });
 });
