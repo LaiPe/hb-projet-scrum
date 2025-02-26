0777afe5622bd33ad38f32638b73a4eab2e1b9f3
Vincent-Gillet <vincent.gillet.vg@gmail.com>
2025-02-26 10:08:35 +0100

    mise à jour admin

---
+++ front/assets/js/admin-generate-user.js
@@ -0,0 +1,16 @@
+let form = document.querySelector("#form");
+
+let username = document.querySelector("#username");
+let username_create = document.querySelector("#username_create");
+let password = document.querySelector("#password");
+
+let password_value = "Lorem";
+
+console.log(password_value);
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    username_create.innerHTML = `${ form.username.value }`;
+    password.innerHTML = `${ password_value }`;
+});
